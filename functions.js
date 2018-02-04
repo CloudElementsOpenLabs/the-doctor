4a6315106a7fcd92c42c377685f81bb631c4e169
Caleb Geene <caleb@cloud-elements.com>
2018-02-03 17:08:53 -0700

    remove unnecessary file

--- functions.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({path: './config.env'});
-const R = require('ramda');
-const createFormulas = require('./src/core/importFormulas');
-
-process.env.ENV = 'production';
-
-createFormulas('/Users/calebgeene/Desktop/formulas-production.json');
