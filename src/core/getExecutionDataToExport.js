47191032cd3ccaf7dd0b869a96c48596358669c9
StephanHanzlik <stephenhanzlik@gmail.com>
2020-02-05 13:48:49 -0700

    Cleaned up getExecutionDataToExport

--- src/core/getExecutionDataToExport.js
+++ src/core/getExecutionDataToExport.js
@@ -2,15 +2,4 @@
 
 const { curry } = require('ramda');
 
-//(fileName)
- module.exports = curry((getData) => {
-        return async function myfunct(id){
-            return await getData(id);
-        };
-    })
-
-// module.exports =  (getData, id) => {
-//     console.log("getData: ", getData)
-//     console.log("id: ", id)
-//     return  getData(id);
-// }
+ module.exports = curry((getData) => async (id)=> await getData(id))
--- src/core/saveExecution.js
+++ src/core/saveExecution.js
@@ -10,10 +10,6 @@ const saveTo = require('./saveTo')
 const makeMessage = name => `Saved Execution: ${name}.`
 const log = forEach(pipe(prop('name'), makeMessage, console.log))
 
-
-//(parms)
 module.exports = async params => {
-    // let execution = await getExecutionDataToExport(getExecution, params.options.name);
-    // getExecutionDataToExport(getExecution, params.options.name), applyVersion(__, params))
     return saveTo(getExecutionDataToExport(getExecution, params.options.name), log, saveToFile, saveToDir)(params)
 }
