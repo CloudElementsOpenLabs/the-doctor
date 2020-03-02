f6ae164a60e81e1a02707ed20ce5d32a64a51ca4
Mike Hirsch <mjhirsch87@gmail.com>
2020-03-02 10:39:37 -0700

    Tidying up

--- src/util/getElements.js
+++ src/util/getElements.js
@@ -11,19 +11,25 @@ const makePath = element => `elements/${element.id}/export`;
 const min = arr => arr.map(x=> {return {key: x.key, id:x.id, name:x.name}});
 
 module.exports = async () => {
+    
     let extended = await getExtendedElements();
     let elements = await getAllElements()
+
     //strip objects down to make them easy to compare as well as filter for the 2 types of elements we care about
     let priv = min(elements.filter(element => element.private==true));
     extended = min(extended.filter(element => element.extended==true));
-    //
+    
     //create a superset of those elements
     let superset = priv.concat(extended);
+
     //return only unique elements
     let uniqElements = uniqBy(superset,'id');
+
     //create the necessary path for GET/{id} for each of the unique elements
     let pathsArr = map(makePath, uniqElements);
+
     //an array of promises that returns the GET/{id} for the elements we care about
     let getByIdArr = pathsArr.map(path => get(path,""));
+
     return await Promise.all(getByIdArr);
 };
