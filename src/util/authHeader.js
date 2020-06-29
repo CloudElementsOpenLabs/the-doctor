377025295ee737da44be4f365423e8db4c456c24
PradheepKumarA <pradheep.arul@gmail.com>
2020-06-29 07:12:35 +0530

    EL-7696 - Compatible change to Support Authorization Header in Doctor Service

--- src/util/authHeader.js
+++ src/util/authHeader.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const production = 'production';
+const { isEmpty, isNil } = require('ramda');
 
-module.exports = () => `User ${process.env.USER_SECRET}, Organization ${process.env.ORG_SECRET}`;
+
+module.exports = () => {
+    if (isNil(process.env.AUTHENTICATION) || isEmpty(process.env.AUTHENTICATION)) {
+        return `User ${process.env.USER_SECRET}, Organization ${process.env.ORG_SECRET}`;
+    }
+    return process.env.AUTHENTICATION;
+}
