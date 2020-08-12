dc13ce65c777de82763bfe0c91b6746570fbc65d
Mohd Rafi <mohdrafi188@gmail.com>
2020-08-12 13:38:38 +0530

    ENG-213 Fixed for the VDR download failure for non-existing VDRs

--- src/core/vdrs/download/downloadVdrs.js
+++ src/core/vdrs/download/downloadVdrs.js
@@ -1,22 +1,25 @@
 'use strict';
 
-const {forEachObjIndexed, type, pipeP, __} = require('ramda');
+const {forEachObjIndexed, type, pipeP, __, join} = require('ramda');
 const applyVersion = require('../../../util/applyVersion');
 const saveToFile = require('../../../util/saveToFile');
 const saveToDir = require('./saveVdrsToDir');
 const saveTo = require('../../saveTo');
 const getVdrNames = require('./getVdrNames');
 const exportVdrs = require('./exportVdrs');
+const applyQuotes = require('../../../util/quoteString');
 
 const getData = async (vdrName, jobId, processId) => {
+  let param = '';
   let vdrNames = [];
   if (type(vdrName) === 'String') {
     vdrNames = vdrName.split(',');
+    param = {where: 'objectName in (' + applyQuotes(join(', ', vdrNames)) + ')'};
   } else if (Array.isArray(vdrName)) {
     vdrNames = vdrName.map((vdr) => vdr.name);
-  } else {
-    vdrNames = await getVdrNames();
+    param = {where: 'objectName in (' + applyQuotes(join(', ', vdrNames)) + ')'};
   }
+  vdrNames = await getVdrNames(param);
   const exportData = await exportVdrs(vdrNames, jobId, processId);
   return exportData;
 };
