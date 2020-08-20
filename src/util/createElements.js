5dde104bc0ae3fbcfe9096608ba54536c679ce00
Mohd Rafi <mohdrafi188@gmail.com>
2020-08-20 20:31:09 +0530

    ENG-387 Added support for extending system element for resources

--- src/util/createElements.js
+++ src/util/createElements.js
@@ -1,32 +1,47 @@
 'use strict';
-const {isNil, propEq, findIndex, isEmpty, equals} = require('ramda');
+const {isNil, isEmpty, equals, concat, find, filter, propOr, pipe, pluck, uniq, join, has} = require('ramda');
 const {emitter, EventTopic} = require('../events/emitter');
 const constructEvent = require('../events/construct-event');
 const {isJobCancelled, removeCancelledJobId} = require('../events/cancelled-job');
 const {Assets, ArtifactStatus} = require('../constants/artifact');
-
-const getElements = require('./getElements');
+const applyQuotes = require('./quoteString');
+const getExtendedElements = require('./getExtendedElements');
+const getPrivateElements = require('./getPrivateElements');
 const get = require('./get');
-const createElement = require('./post')('elements');
 const post = require('./post');
+const createElement = require('./post')('elements');
 const makePath = (element) => `elements/${element.key}`;
 const update = require('./update');
-const min = (arr) => arr.map((x) => ({methodPath: x.path + x.method}));
 const isNilOrEmpty = (val) => isNil(val) || isEmpty(val);
 
-const isNewElementToCreate = async (allElements, elementToImport) => {
-  // Here we need to identify whether the element is already present or not
-  // Get all the elements at user account level and check the existence of the element
-  const existingElements = !isNilOrEmpty(allElements)
-    ? allElements.filter(
-        (element) => equals(element.key, elementToImport.key) && equals(element.private, elementToImport.private),
-      )
+const fetchAllElements = async (elementsToImport) => {
+  const elementsKeyString = !isNilOrEmpty(elementsToImport)
+    ? pipe(pluck('key'), uniq, join(','))(elementsToImport)
+    : '';
+  const privateElements = await getPrivateElements(elementsKeyString);
+  const allExtendedElements = await getExtendedElements(elementsKeyString);
+  const extendedElements = !isNilOrEmpty(allExtendedElements)
+    ? allExtendedElements.filter((element) => element.extended && !element.private)
     : [];
-  return isNilOrEmpty(existingElements);
+  return concat(privateElements, extendedElements);
+};
+
+const fetchExtendedAndPrivateResources = async (existingElementId, elementKey) => {
+  // GET /resources will always return private element resources if exists
+  let extendedResources;
+  if (isNilOrEmpty(existingElementId)) {
+    extendedResources = await get(`elements/${elementKey}/resources`, '');
+  } else {
+    const extendedElement = await get(`elements/${existingElementId}`, '');
+    extendedResources = propOr([], 'resources')(extendedElement);
+  }
+  return !isNilOrEmpty(extendedResources)
+    ? filter((resource) => !equals(resource.ownerAccountId, 1), extendedResources)
+    : [];
 };
 
 module.exports = async (elements, jobId, processId) => {
-  const allElements = await getElements();
+  const allElements = await fetchAllElements(elements);
   let uploadPromise = await elements.map(async (element) => {
     try {
       if (isJobCancelled(jobId)) {
@@ -37,59 +52,83 @@ module.exports = async (elements, jobId, processId) => {
         EventTopic.ASSET_STATUS,
         constructEvent(processId, Assets.ELEMENTS, element.key, ArtifactStatus.INPROGRESS, '', '', false),
       );
-      const isNewElement = await isNewElementToCreate(allElements, element);
-      if (!isNewElement) {
+      // Here we need to identify whether the element is already present or not
+      // Get all the elements at user account level and check the existence of the element
+      const existingElement = !isNilOrEmpty(allElements)
+        ? find((searchElement) =>
+            equals(element.key, searchElement.key) && has('private', element)
+              ? equals(element.private, searchElement.private) && !searchElement.extended
+              : equals(element.extended, searchElement.extended) && !searchElement.private,
+          )(allElements)
+        : [];
+      if (isNilOrEmpty(existingElement)) {
+        // Element doesn't exists in the db for given account
         if (element.private === true || element.actuallyExtended === false) {
-          await update(makePath(element), element);
-          console.log(`Updated Element: ${element.key}`);
-        } else {
-          //extend the element
-          // TODO
-          // GET resources from destination to compare
-          const resources = await get(`elements/${element.key}/resources`, '');
-          const destinationResources = resources.filter((resource) => resource.ownerAccountId !== 1);
-
-          //combination of path + method is uniquq per element per account
-          const compDestination = min(destinationResources);
-          const compOrigin = min(element.resources);
-
-          let toUpload = {create: [], update: []};
-          // 2, Compare GET/resources to the resources in the saved element
-          for (let i = 0; i < compOrigin.length; i++) {
-            const exists = findIndex(propEq('methodPath', compOrigin[i].methodPath))(compDestination);
-            console.log(`Element Exteneded: ${element.key}`);
-            if (exists > -1) {
-              // 3. If resource already exist - PUT
-              console.log('destinationResources[exists].id', destinationResources[exists].id);
-              toUpload.update.push(
-                update(`elements/${element.key}/resources/${destinationResources[exists].id}`, element.resources[i]),
-              );
-              console.log(`Resource Updated:\n Method: ${element.resources[i].method}\n Path: ${element.resources[i].path}`);
-            } else {
-              // 4. If resource doesn't exist - POST
-              toUpload.create.push(post(`elements/${element.key}/resources`, element.resources[i]));
-              console.log(`Resource Created:\n Method: ${element.resources[i].method}\n Path: ${element.resources[i].path}`);
-            }
-          }
-
-          //combine arrays of promises
-          let arrs = toUpload.create.concat(toUpload.update);
-          await Promise.all(arrs);
-        }
-      } else {
-        if (element.private === true || element.actuallyExtended === false) {
-          //create non-extended element
+          // Create non-extended element (Private element)
           await createElement(element);
           console.log(`Created Element: ${element.key}`);
         } else {
-          let toUpload = {create: [], update: []};
-          for (let i = 0; i < element.resources.length; i++) {
-            toUpload.create.push(post(`elements/${element.key}/resources`, element.resources[i]));
-            console.log(`Resource Created:\n Method: ${element.resources[i].method}\n Path: ${element.resources[i].path}`);
+          let promisesList = {createdResources: [], updatedResources: []};
+          if (!isNilOrEmpty(element.resources)) {
+            // If we try to create resource based on element key then it will always
+            // create resource in private element if exists
+            const elementsForKey = await get('elements', {where: "key = " + applyQuotes(element.key)});
+            const systemElementToExtend = !isNilOrEmpty(elementsForKey)
+              ? find((searchElement) =>
+                  equals(element.key, searchElement.key) && has('private', searchElement)
+                    ? !searchElement.private
+                    : false,
+                )(elementsForKey)
+              : [];
+
+            if (isNilOrEmpty(systemElementToExtend) && isNilOrEmpty(systemElementToExtend.id)) {
+              element.resources.forEach((resource) => {
+                promisesList.createdResources.push(post(`elements/${element.key}/resources`, resource));
+                console.log(`Resource Created: ${resource.method} - ${resource.path}`);
+              });
+            } else {
+              element.resources.forEach((resource) => {
+                promisesList.createdResources.push(post(`elements/${systemElementToExtend.id}/resources`, resource));
+                console.log(`Resource Created: ${resource.method} - ${resource.path}`);
+              });
+            }
           }
-          //combine arrays of promises
-          let arrs = toUpload.create.concat(toUpload.update);
-          await Promise.all(arrs);
+          // Combine both the promises list and resolve all
+          const allPromisesToResolve = concat(promisesList.createdResources, promisesList.updatedResources);
+          await Promise.all(allPromisesToResolve);
+        }
+      } else {
+        // Element exists in the db for given account
+        if (element.private === true || element.actuallyExtended === false) {
+          // Create non-extended element (Private element)
+          await update(makePath(element), element);
+          console.log(`Updated Element: ${element.key}`);
+        } else {
+          // Extend the element resources and element configurations (TODO)
+          let promisesList = {createdResources: [], updatedResources: []};
+          const extendedResources = await fetchExtendedAndPrivateResources(existingElement.id, element.key);
+          if (!isNilOrEmpty(element.resources)) {
+            element.resources.forEach((resource) => {
+              const existingResource = find(
+                (extendedResource) =>
+                  equals(extendedResource.method, resource.method) &&
+                  equals(extendedResource.path, resource.path) &&
+                  equals(extendedResource.type, resource.type),
+              )(extendedResources);
+              if (isNilOrEmpty(existingResource)) {
+                promisesList.createdResources.push(post(`elements/${existingElement.id}/resources`, resource));
+                console.log(`Resource Created: ${resource.method} - ${resource.path}`);
+              } else {
+                promisesList.updatedResources.push(
+                  update(`elements/${existingElement.id}/resources/${existingResource.id}`, resource),
+                );
+                console.log(`Resource Updated: ${resource.method} - ${resource.path}`);
+              }
+            });
+          }
+          // Combine both the promises list and resolve all
+          const allPromisesToResolve = concat(promisesList.createdResources, promisesList.updatedResources);
+          await Promise.all(allPromisesToResolve);
         }
       }
       emitter.emit(
