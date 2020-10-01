218838505753703b314020ca812b47cfd26763cd
Mohd Rafi <mohdrafi188@gmail.com>
2020-10-01 14:06:08 +0530

    ENG-1156 Fixed download of all assets files

--- src/core/importBackup.js
+++ src/core/importBackup.js
@@ -1,36 +1,29 @@
 'use strict';
 const {pipe, pipeP, cond, prop, isNil, not, useWith} = require('ramda');
-const importElements = require('./importElements')
-const importVdrs = require('./vdrs/upload/uploadMultipleVdrs')
-const importFormulas = require('./importFormulas')
+const importElements = require('./importElements');
+const importVdrs = require('./vdrs/upload/uploadMultipleVdrs');
+const importFormulas = require('./importFormulas');
 const createObjectDefinitions = require('../util/createObjectDefinitions');
 const createTransformations = require('../util/createTransformations');
 const createFormulas = require('../util/createFormulas');
 const createElements = require('../util/createElements');
 const readFile = require('../util/readFile');
 
-const importBackupFromFile = async fileData => {
-    await createElements(fileData.elements)
-    createObjectDefinitions(fileData).then(() => {
-        createTransformations(fileData)
-    });
-    await createFormulas(fileData.formulas)
-}
+const importBackupFromFile = async (fileData) => {
+  await createElements(fileData.elements);
+  createObjectDefinitions(fileData).then(() => {
+    createTransformations(fileData);
+  });
+  await createFormulas(fileData.formulas);
+};
 
-const importBackupFromDir = async parms => {
-    await importElements({...parms, dir: `${parms.dir}/elements`})
-    await importVdrs({...parms, dir: `${parms.dir}/vdrs`})
-    await importFormulas({...parms, dir: `${parms.dir}/formulas`})
-}
+const importBackupFromDir = async (parms) => {
+  await importElements({...parms, dir: `${parms.dir}/elements`});
+  await importVdrs({...parms, dir: `${parms.dir}/vdrs`});
+  await importFormulas({...parms, dir: `${parms.dir}/formulas`});
+};
 
-//(fileName)
 module.exports = cond([
-    [ 
-      pipe(prop('file'), isNil, not),
-      pipeP(useWith(readFile, [prop('file')]), importBackupFromFile)
-    ],
-    [
-      pipe(prop('dir'), isNil, not),
-      importBackupFromDir
-    ]
-  ])
+  [pipe(prop('file'), isNil, not), pipeP(useWith(readFile, [prop('file')]), importBackupFromFile)],
+  [pipe(prop('dir'), isNil, not), importBackupFromDir],
+]);
--- src/core/saveAll.js
+++ src/core/saveAll.js
@@ -1,40 +1,30 @@
 'use strict';
+const {pipe, prop, map} = require('ramda');
+const getFormulas = require('../util/getFormulas');
+const getVdrs = require('./vdrs/download/getVdrs');
+const getElements = require('../util/getElements');
+const saveToFile = require('../util/saveToFile');
+const saveToDir = require('../util/saveBackupToDir');
+const saveTo = require('./saveTo');
 
-const {pipe, prop, map, forEach} = require('ramda')
-const getFormulas = require('../util/getFormulas')
-const findTransformations = require('../util/findTransformations')
-const get = require('../util/get')
-const getElements = require('../util/getElements')
-const saveToFile = require('../util/saveToFile')
-const saveToDir = require('../util/saveBackupToDir')
-const saveTo = require('./saveTo')
+const makeMessage = (object) => (name) => `Saved ${object}: ${name}.`;
+const log = (object, propName) => map(pipe(prop(propName), makeMessage(object), console.log));
 
-const makeMessage = (object) => name => `Saved ${object}: ${name}.`
-const log = (object, propName) => map(pipe(prop(propName), makeMessage(object), console.log))
-
-const logAll = async data => {
-    const elements = await data.elements
-    log('Element','name')(elements)
-    const objects = await data.objectDefinitions
-    log('Object','objectName')(objects)
-    const transformations = await data.transformations
-    forEach(element => {
-        forEach(transformation => {
-          console.log(`Saved Transformation: ${prop('objectName',transformation)} - ${element}.`)
-        })(transformations[element])
-      })(transformations)
-    const formulas = await data.formulas
-    log('Formula','name')(formulas)
-}
+const logAll = async (data) => {
+  const elements = await data.elements;
+  log('Element', 'name')(elements);
+  const vdrs = await data.vdrs;
+  log('vdrs', 'vdrName')(vdrs);
+  const formulas = await data.formulas;
+  log('Formula', 'name')(formulas);
+};
 
 const getData = async () => {
-    return {
-        formulas: await getFormulas(),
-        transformations: await findTransformations(),
-        objectDefinitions: await get(`organizations/objects/definitions`,""),
-        elements: await getElements()
-    }
-  }
+  return {
+    elements: await getElements(),
+    formulas: await getFormulas(),
+    vdrs: await getVdrs(),
+  };
+};
 
-//(parms)
-module.exports = saveTo(getData, logAll, saveToFile, saveToDir)
+module.exports = saveTo(getData, logAll, saveToFile, saveToDir);
