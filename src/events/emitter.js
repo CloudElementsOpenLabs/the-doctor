f7a2e084a6197ef7ccebd45c1d191af0e3f6634b
Mohd Rafi <mohdrafi188@gmail.com>
2020-10-06 10:52:52 +0530

    ENG-1193 Added JOB_CREATED event topic

--- src/events/emitter.js
+++ src/events/emitter.js
@@ -3,19 +3,19 @@ const EventEmitter = require('events');
 let emitter;
 
 const singleton = () => {
-    if (!emitter) {
-        emitter = new EventEmitter();
-    }
-    return emitter;
+  if (!emitter) {
+    emitter = new EventEmitter();
+  }
+  return emitter;
 };
 
 const EventTopic = {
-    ASSET_STATUS: 'ASSERT_STATUS',
-    JOB_CANCELLED: 'JOB_CANCELLED'
-}
-
+  ASSET_STATUS: 'ASSERT_STATUS',
+  JOB_CANCELLED: 'JOB_CANCELLED',
+  JOB_CREATED: 'JOB_CREATED',
+};
 
 module.exports = {
-    emitter: singleton(),
-    EventTopic
+  emitter: singleton(),
+  EventTopic,
 };
