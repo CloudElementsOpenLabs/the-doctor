8546aeea47f0ebdd08786a5a1ef84c009568e6c5
Mohd Rafi Uddin <35064158+MohdRafiUddin@users.noreply.github.com>
2020-10-08 12:31:48 +0530

    ENG-1193 | Passing userId to JOB_CANCELLED event (#52)
    * ENG-1193 Passing userId to JOB_CANCELLED event
    
    * ENG-1193 Bump beta version

--- src/events/event-listener.js
+++ src/events/event-listener.js
@@ -1,22 +1,20 @@
-const { addCancelledJobId } = require('./cancelled-job');
-const { emitter, EventTopic } = require('./emitter');
-
+const {addCancelledJobId} = require('./cancelled-job');
+const {emitter, EventTopic} = require('./emitter');
 
 // Doctor core listen for the events emitted from the doctor service and holds it in the global set.
 // Doctor checks whether the job is cancelled by checkig if that job id present in the global set
-const jobCancelledHandler = (jobId) => {
-    addCancelledJobId(jobId);
-}
+const jobCancelledHandler = (args) => {
+  addCancelledJobId(args.jobId);
+};
 
 let listenerLoaded;
 const addListener = () => {
-    if (!listenerLoaded) {
-        emitter.on(EventTopic.JOB_CANCELLED, jobCancelledHandler);
-        listenerLoaded = true;
-    }
+  if (!listenerLoaded) {
+    emitter.on(EventTopic.JOB_CANCELLED, jobCancelledHandler);
+    listenerLoaded = true;
+  }
 };
 
-
 module.exports = {
-    addListener
+  addListener,
 };
