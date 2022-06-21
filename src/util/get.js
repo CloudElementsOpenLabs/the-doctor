f4287c2712ac4ec15fdbc4c58a8ec3b0e49b2f8c
Mukund <mukundbayyaram@gmail.com>
2022-06-21 14:52:35 +0530

    fix: added missing required imports (ENGCE-13861) (#67)
    * fix: added missing required imports (ENGCE-13861)
    
    * fix: added code owners
    
    * Update CODEOWNERS

--- src/util/get.js
+++ src/util/get.js
@@ -1,21 +1,22 @@
 'use strict';
 
+const {curry, pathSatisfies, test} = require('ramda');
 const rp = require('request-promise');
 const authHeader = require('./authHeader');
 const baseUrl = require('./baseUrl');
-const {curry, test} = require('ramda');
 
 module.exports = curry(async (path,qs) => {
   let options = {
     json: true,
     headers: {
-        Authorization: authHeader(),
+      Authorization: authHeader(),
     },
     url: baseUrl(path),
     method: "GET",
     strictSSL: false
   };
   qs?options.qs = qs:'';
+  
   try {
     return await rp(options);
   } catch (err) {
